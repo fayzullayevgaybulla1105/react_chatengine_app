452128bab881b2c58655b22b518b5cbf8db8dd55
fayzullayevgaybulla <fayzullayevgn@gmail.com>
2022-03-07 18:33:32 +0500

    useToken.jsx file

---
+++ src/Hooks/useToken.jsx
@@ -0,0 +1,12 @@
+
+import React from 'react';
+
+import { Context } from '../Context/Authentication.jsx';
+
+function useToken() {
+	const ctx = React.useContext(Context);
+
+	return [ctx.state, ctx.setState];
+}
+
+export default useToken;
